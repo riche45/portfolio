1d1796120d31d890523c6506976a7ce61a9a05c5
Richard Garcia <tayrona7@hotmail.com>
2023-06-05 10:31:35 -0500

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path("", render_posts, name="posts"),
-    path("<int:post_id>", post_detail, name="post_detail"),
+    path("<int:post_id>/", post_detail, name="post_detail"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,4 @@ from .models import Project
 # Create your views here.
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'home.html', {'projects': projects}) 
+    return render(request, 'index.html', {'projects': projects}) 
